595de9c38e20d304792088f49b9ea10fa03eadc6
emanhussein <emanhussein@172.30.5.192>
2019-04-24 19:13:27 -0400

    UserPhoto table and corresponding files added

--- NetBeansProjects/WeCommUnity/src/java/edu/vt/controllers/CommunityController.java
+++ NetBeansProjects/WeCommUnity/src/java/edu/vt/controllers/CommunityController.java
@@ -2,168 +2,197 @@ package edu.vt.controllers;
 
 import edu.vt.EntityBeans.Community;
 import edu.vt.controllers.util.JsfUtil;
-import edu.vt.controllers.util.JsfUtil.PersistAction;
+import edu.vt.controllers.util.PaginationHelper;
 import edu.vt.FacadeBeans.CommunityFacade;
-import edu.vt.globals.Methods;
 
 import java.io.Serializable;
-import java.util.List;
 import java.util.ResourceBundle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.EJB;
-import javax.ejb.EJBException;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
 import javax.faces.convert.FacesConverter;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
+import javax.faces.model.SelectItem;
+
 
 @Named("communityController")
 @SessionScoped
 public class CommunityController implements Serializable {
 
-    @EJB
-    private edu.vt.FacadeBeans.CommunityFacade communityFacade;
-    private List<Community> userCommunitiesList = null;
-    private List<Community> allCommunitiesList = null;
-    private Community selected;
 
-    /**
-     * ************************************************************************************
-     */
-    public void setCommunityFacade(CommunityFacade cFacade) {
-        this.communityFacade = cFacade;
-    }
+    private Community current;
+    private DataModel items = null;
+    @EJB private edu.vt.FacadeBeans.CommunityFacade ejbFacade;
+    private PaginationHelper pagination;
+    private int selectedItemIndex;
 
-    private CommunityFacade getCommunityFacade() {
-        return communityFacade;
-    }
-
-    /**
-     * ************************************************************************************
-     */
-    public void setUserCommunitiesList(List<Community> userCommunitiesList) {
-        this.userCommunitiesList = userCommunitiesList;
-    }
-
-    //need to get the user id to retrieve the communities he/she joined
-    public List<Community> getuserCommunitiesList() {
-        if (userCommunitiesList == null) {
-             /*
-            user_id (database primary key) was put into the SessionMap in the
-            initializeSessionMap() method in LoginManager upon user's sign in.
-             */
-            int userPrimaryKey = (int) Methods.sessionMap().get("user_id");
-
-            userCommunitiesList = getCommunityFacade().findUserCommunitiesByUserPrimaryKey(userPrimaryKey);
-        }
-        return userCommunitiesList;
-    }
-
-    /**
-     * ************************************************************************************
-     */
-    public void setAllCommunitiesList(List<Community> allCommunitiesList) {
-        this.allCommunitiesList = allCommunitiesList;
-    }
-
-    public List<Community> getAllCommunitiesList() {
-        if (allCommunitiesList == null) {
-            allCommunitiesList = getCommunityFacade().findAll();
-        }
-        return allCommunitiesList;
-    }
-
-    /**
-     * ************************************************************************************
-     */
-    public Community getSelected() {
-        return selected;
-    }
-    public void setSelected(Community selected) {
-        this.selected = selected;
-    }
-
-    /**
-     * ************************************************************************************
-     */
     public CommunityController() {
     }
 
-    protected void setEmbeddableKeys() {
+    public Community getSelected() {
+        if (current == null) {
+            current = new Community();
+            selectedItemIndex = -1;
+        }
+        return current;
     }
 
-    protected void initializeEmbeddableKey() {
+    private CommunityFacade getFacade() {
+        return ejbFacade;
+    }
+    public PaginationHelper getPagination() {
+        if (pagination == null) {
+            pagination = new PaginationHelper(10) {
+
+                @Override
+                public int getItemsCount() {
+                    return getFacade().count();
+                }
+
+                @Override
+                public DataModel createPageDataModel() {
+                    return new ListDataModel(getFacade().findRange(new int[]{getPageFirstItem(), getPageFirstItem()+getPageSize()}));
+                }
+            };
+        }
+        return pagination;
     }
 
-    public Community prepareCreate() {
-        selected = new Community();
-        initializeEmbeddableKey();
-        return selected;
+    public String prepareList() {
+        recreateModel();
+        return "List";
     }
 
-    public void create() {
-        persist(PersistAction.CREATE, ResourceBundle.getBundle("/Bundle").getString("CommunityCreated"));
-        if (!JsfUtil.isValidationFailed()) {
-            userCommunitiesList = null;    // Invalidate list of userCommunitiesList to trigger re-query.
+    public String prepareView() {
+        current = (Community)getItems().getRowData();
+        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
+        return "View";
+    }
+
+    public String prepareCreate() {
+        current = new Community();
+        selectedItemIndex = -1;
+        return "Create";
+    }
+
+    public String create() {
+        try {
+            getFacade().create(current);
+            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("CommunityCreated"));
+            return prepareCreate();
+        } catch (Exception e) {
+            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+            return null;
         }
     }
 
-    public void update() {
-        persist(PersistAction.UPDATE, ResourceBundle.getBundle("/Bundle").getString("CommunityUpdated"));
+    public String prepareEdit() {
+        current = (Community)getItems().getRowData();
+        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
+        return "Edit";
     }
 
-    public void destroy() {
-        persist(PersistAction.DELETE, ResourceBundle.getBundle("/Bundle").getString("CommunityDeleted"));
-        if (!JsfUtil.isValidationFailed()) {
-            selected = null; // Remove selection
-            userCommunitiesList = null;    // Invalidate list of userCommunitiesList to trigger re-query.
+    public String update() {
+        try {
+            getFacade().edit(current);
+            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("CommunityUpdated"));
+            return "View";
+        } catch (Exception e) {
+            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+            return null;
         }
     }
 
-    private void persist(PersistAction persistAction, String successMessage) {
-        if (selected != null) {
-            setEmbeddableKeys();
-            try {
-                if (persistAction != PersistAction.DELETE) {
-                    getCommunityFacade().edit(selected);
-                } else {
-                    getCommunityFacade().remove(selected);
-                }
-                JsfUtil.addSuccessMessage(successMessage);
-            } catch (EJBException ex) {
-                String msg = "";
-                Throwable cause = ex.getCause();
-                if (cause != null) {
-                    msg = cause.getLocalizedMessage();
-                }
-                if (msg.length() > 0) {
-                    JsfUtil.addErrorMessage(msg);
-                } else {
-                    JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-                }
-            } catch (Exception ex) {
-                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, null, ex);
-                JsfUtil.addErrorMessage(ex, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+    public String destroy() {
+        current = (Community)getItems().getRowData();
+        selectedItemIndex = pagination.getPageFirstItem() + getItems().getRowIndex();
+        performDestroy();
+        recreatePagination();
+        recreateModel();
+        return "List";
+    }
+
+    public String destroyAndView() {
+        performDestroy();
+        recreateModel();
+        updateCurrentItem();
+        if (selectedItemIndex >= 0) {
+            return "View";
+        } else {
+            // all items were removed - go back to list
+            recreateModel();
+            return "List";
+        }
+    }
+
+    private void performDestroy() {
+        try {
+            getFacade().remove(current);
+            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("CommunityDeleted"));
+        } catch (Exception e) {
+            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+        }
+    }
+
+    private void updateCurrentItem() {
+        int count = getFacade().count();
+        if (selectedItemIndex >= count) {
+            // selected index cannot be bigger than number of items:
+            selectedItemIndex = count-1;
+            // go to previous page if last page disappeared:
+            if (pagination.getPageFirstItem() >= count) {
+                pagination.previousPage();
             }
         }
+        if (selectedItemIndex >= 0) {
+            current = getFacade().findRange(new int[]{selectedItemIndex, selectedItemIndex+1}).get(0);
+        }
+    }
+
+    public DataModel getItems() {
+        if (items == null) {
+            items = getPagination().createPageDataModel();
+        }
+        return items;
+    }
+
+    private void recreateModel() {
+        items = null;
+    }
+
+    private void recreatePagination() {
+        pagination = null;
+    }
+
+    public String next() {
+        getPagination().nextPage();
+        recreateModel();
+        return "List";
+    }
+
+    public String previous() {
+        getPagination().previousPage();
+        recreateModel();
+        return "List";
+    }
+
+    public SelectItem[] getItemsAvailableSelectMany() {
+        return JsfUtil.getSelectItems(ejbFacade.findAll(), false);
+    }
+
+    public SelectItem[] getItemsAvailableSelectOne() {
+        return JsfUtil.getSelectItems(ejbFacade.findAll(), true);
     }
 
     public Community getCommunity(java.lang.Integer id) {
-        return getCommunityFacade().find(id);
+        return ejbFacade.find(id);
     }
 
-    public List<Community> getuserCommunitiesListAvailableSelectMany() {
-        return getCommunityFacade().findAll();
-    }
-
-    public List<Community> getuserCommunitiesListAvailableSelectOne() {
-        return getCommunityFacade().findAll();
-    }
-
-    @FacesConverter(forClass = Community.class)
+    @FacesConverter(forClass=Community.class)
     public static class CommunityControllerConverter implements Converter {
 
         @Override
@@ -171,7 +200,7 @@ public class CommunityController implements Serializable {
             if (value == null || value.length() == 0) {
                 return null;
             }
-            CommunityController controller = (CommunityController) facesContext.getApplication().getELResolver().
+            CommunityController controller = (CommunityController)facesContext.getApplication().getELResolver().
                     getValue(facesContext.getELContext(), null, "communityController");
             return controller.getCommunity(getKey(value));
         }
@@ -197,8 +226,7 @@ public class CommunityController implements Serializable {
                 Community o = (Community) object;
                 return getStringKey(o.getId());
             } else {
-                Logger.getLogger(this.getClass().getName()).log(Level.SEVERE, "object {0} is of type {1}; expected type: {2}", new Object[]{object, object.getClass().getName(), Community.class.getName()});
-                return null;
+                throw new IllegalArgumentException("object " + object + " is of type " + object.getClass().getName() + "; expected type: "+Community.class.getName());
             }
         }
 
